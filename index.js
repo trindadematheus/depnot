503a5e1983d5ad5a6c6ce58979210761b4daf52a
Matheus Trindade <trindadematheus27@gmail.com>
2022-07-12 23:08:58 -0300

    first version

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+
+const args = process.argv.slice(2, process.argv.length);
+const locale = process.env.INIT_CWD;
+
+const manager = args[0];
+const libname = args[1];
+// const withDependencies = args[2];
+
+const packageObj = JSON.parse(
+  fs.readFileSync(path.resolve(locale, "package.json"), "utf8")
+);
+
+const dependenciesStr = Object.keys(packageObj.dependencies).reduce(
+  (acc, curr) => {
+    if (curr.includes(libname)) {
+      acc += `${curr} `;
+    }
+
+    return acc;
+  },
+  ""
+);
+
+let cmdText = "";
+
+switch (manager) {
+  case "yarn":
+    cmdText = `yarn remove ${dependenciesStr}`;
+    break;
+  case "npm":
+    cmdText = `npm uninstall ${dependenciesStr}`;
+    break;
+  case "pnpm":
+    cmdText = `pnpm uninstall ${dependenciesStr}`;
+    break;
+  default:
+    cmdText = `npm uninstall ${dependenciesStr}`;
+    break;
+}
+
+console.log(`\x1b[32m SELECTED DEPENDENCIES: \x1b[0m`);
+console.log(`\x1b[33m ${dependenciesStr} \x1b[0m`);
+
+exec(cmdText);
